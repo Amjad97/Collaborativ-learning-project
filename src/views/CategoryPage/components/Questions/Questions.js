5cd91a7db2a84dbc234ed71f425c3805ffbbc8db
[Amjad97] <[Amjad_97@outlook.sa]>
2019-11-22 16:13:18 +0200

    fix ask question button

--- src/views/CategoryPage/components/Questions/Questions.js
+++ src/views/CategoryPage/components/Questions/Questions.js
@@ -24,254 +24,254 @@ function Questions() {
   };
 
   return (
+    <div style={{ padding: "50px 0px" }}>
       <div
-        style={{ padding: "50px 0px" }}
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          margin: "0px 55px",
+          alignItems: "baseline"
+        }}
+      >
+        <h2 className="title">Questions</h2>
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ height: "max-content" }}
+          onClick={handleClickOpen}
+        >
+          Ask Question
+        </Button>
+        <QuestionDialog open={open} handleClose={handleClose} />
+      </div>
+      <div
+        style={{ padding: "0px 40px" }}
         onClick={() => history.push("/question-page")}
       >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            margin: "0px 55px",
-            alignItems: "baseline"
-          }}
-        >
-          <h2 className="title">Questions</h2>
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ height: "max-content" }}
-            onClick={handleClickOpen}
-          >
-            Ask Question
-          </Button>
-          <QuestionDialog open={open} handleClose={handleClose} />
-        </div>
-        <div style={{ padding: "0px 40px" }}>
-          <List>
-            <ListItem style={{ flex: 1 }}>
-              <div style={{ display: "block", flex: 0.1 }}>
-                <div style={{ textAlign: "center" }}>0</div>
-                <div style={{ textAlign: "center" }}>votes</div>
-                <div style={{ textAlign: "center" }}>0</div>
-                <div style={{ textAlign: "center" }}>answers</div>
-              </div>
-              <div style={{ flex: 0.85 }}>
-                <ListItemText
-                  primary="Question Title"
-                  secondary="lorem ipsum dolor sit amet, consetetur 
+        <List>
+          <ListItem style={{ flex: 1 }}>
+            <div style={{ display: "block", flex: 0.1 }}>
+              <div style={{ textAlign: "center" }}>0</div>
+              <div style={{ textAlign: "center" }}>votes</div>
+              <div style={{ textAlign: "center" }}>0</div>
+              <div style={{ textAlign: "center" }}>answers</div>
+            </div>
+            <div style={{ flex: 0.85 }}>
+              <ListItemText
+                primary="Question Title"
+                secondary="lorem ipsum dolor sit amet, consetetur 
             sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
             aliquyam erat, sed diam.  lorem ipsum dolor sit amet, consetetur 
             sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
             aliquyam erat, sed diam.  lorem ipsum"
+              />
+              <div
+                style={{
+                  backgroundColor: "grey",
+                  padding: 10,
+                  width: "fit-content",
+                  margin: 5,
+                  borderRadius: 10,
+                  color: "white",
+                  fontSize: 12
+                }}
+              >
+                Web Development
+              </div>
+            </div>
+            <div
+              style={{
+                flex: 0.05,
+                display: "grid",
+                justifyContent: "center"
+              }}
+            >
+              <div>
+                <img
+                  src={image}
+                  style={{ width: "35px", height: "35px", margin: 10 }}
+                  alt="avatar"
                 />
-                <div
-                  style={{
-                    backgroundColor: "grey",
-                    padding: 10,
-                    width: "fit-content",
-                    margin: 5,
-                    borderRadius: 10,
-                    color: "white",
-                    fontSize: 12
-                  }}
-                >
-                  Web Development
-                </div>
               </div>
               <div
                 style={{
-                  flex: 0.05,
-                  display: "grid",
-                  justifyContent: "center"
+                  fontSize: "10px",
+                  color: "grey"
                 }}
               >
-                <div>
-                  <img
-                    src={image}
-                    style={{ width: "35px", height: "35px", margin: 10 }}
-                    alt="avatar"
-                  />
-                </div>
-                <div
-                  style={{
-                    fontSize: "10px",
-                    color: "grey"
-                  }}
-                >
-                  JOHN DOE
-                </div>
+                JOHN DOE
               </div>
-            </ListItem>
-            <Divider />
-            <ListItem style={{ flex: 1 }}>
-              <div style={{ display: "block", flex: 0.1 }}>
-                <div style={{ textAlign: "center" }}>0</div>
-                <div style={{ textAlign: "center" }}>votes</div>
-                <div style={{ textAlign: "center" }}>0</div>
-                <div style={{ textAlign: "center" }}>answers</div>
-              </div>
-              <div style={{ flex: 0.85 }}>
-                <ListItemText
-                  primary="Question Title"
-                  secondary="lorem ipsum dolor sit amet, consetetur 
+            </div>
+          </ListItem>
+          <Divider />
+          <ListItem style={{ flex: 1 }}>
+            <div style={{ display: "block", flex: 0.1 }}>
+              <div style={{ textAlign: "center" }}>0</div>
+              <div style={{ textAlign: "center" }}>votes</div>
+              <div style={{ textAlign: "center" }}>0</div>
+              <div style={{ textAlign: "center" }}>answers</div>
+            </div>
+            <div style={{ flex: 0.85 }}>
+              <ListItemText
+                primary="Question Title"
+                secondary="lorem ipsum dolor sit amet, consetetur 
             sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
             aliquyam erat, sed diam.  lorem ipsum dolor sit amet, consetetur 
             sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
             aliquyam erat, sed diam.  lorem ipsum"
+              />
+              <div
+                style={{
+                  backgroundColor: "grey",
+                  padding: 10,
+                  width: "fit-content",
+                  margin: 5,
+                  borderRadius: 10,
+                  color: "white",
+                  fontSize: 12
+                }}
+              >
+                Web Development
+              </div>
+            </div>
+            <div
+              style={{
+                flex: 0.05,
+                display: "grid",
+                justifyContent: "center"
+              }}
+            >
+              <div>
+                <img
+                  src={image}
+                  style={{ width: "35px", height: "35px", margin: 10 }}
+                  alt="avatar"
                 />
-                <div
-                  style={{
-                    backgroundColor: "grey",
-                    padding: 10,
-                    width: "fit-content",
-                    margin: 5,
-                    borderRadius: 10,
-                    color: "white",
-                    fontSize: 12
-                  }}
-                >
-                  Web Development
-                </div>
               </div>
               <div
                 style={{
-                  flex: 0.05,
-                  display: "grid",
-                  justifyContent: "center"
+                  fontSize: "10px",
+                  color: "grey"
                 }}
               >
-                <div>
-                  <img
-                    src={image}
-                    style={{ width: "35px", height: "35px", margin: 10 }}
-                    alt="avatar"
-                  />
-                </div>
-                <div
-                  style={{
-                    fontSize: "10px",
-                    color: "grey"
-                  }}
-                >
-                  JOHN DOE
-                </div>
+                JOHN DOE
               </div>
-            </ListItem>
-            <Divider />
-            <ListItem style={{ flex: 1 }}>
-              <div style={{ display: "block", flex: 0.1 }}>
-                <div style={{ textAlign: "center" }}>0</div>
-                <div style={{ textAlign: "center" }}>votes</div>
-                <div style={{ textAlign: "center" }}>0</div>
-                <div style={{ textAlign: "center" }}>answers</div>
-              </div>
-              <div style={{ flex: 0.85 }}>
-                <ListItemText
-                  primary="Question Title"
-                  secondary="lorem ipsum dolor sit amet, consetetur 
+            </div>
+          </ListItem>
+          <Divider />
+          <ListItem style={{ flex: 1 }}>
+            <div style={{ display: "block", flex: 0.1 }}>
+              <div style={{ textAlign: "center" }}>0</div>
+              <div style={{ textAlign: "center" }}>votes</div>
+              <div style={{ textAlign: "center" }}>0</div>
+              <div style={{ textAlign: "center" }}>answers</div>
+            </div>
+            <div style={{ flex: 0.85 }}>
+              <ListItemText
+                primary="Question Title"
+                secondary="lorem ipsum dolor sit amet, consetetur 
             sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
             aliquyam erat, sed diam.  lorem ipsum dolor sit amet, consetetur 
             sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
             aliquyam erat, sed diam.  lorem ipsum"
+              />
+              <div
+                style={{
+                  backgroundColor: "grey",
+                  padding: 10,
+                  width: "fit-content",
+                  margin: 5,
+                  borderRadius: 10,
+                  color: "white",
+                  fontSize: 12
+                }}
+              >
+                Web Development
+              </div>
+            </div>
+            <div
+              style={{
+                flex: 0.05,
+                display: "grid",
+                justifyContent: "center"
+              }}
+            >
+              <div>
+                <img
+                  src={image}
+                  style={{ width: "35px", height: "35px", margin: 10 }}
+                  alt="avatar"
                 />
-                <div
-                  style={{
-                    backgroundColor: "grey",
-                    padding: 10,
-                    width: "fit-content",
-                    margin: 5,
-                    borderRadius: 10,
-                    color: "white",
-                    fontSize: 12
-                  }}
-                >
-                  Web Development
-                </div>
               </div>
               <div
                 style={{
-                  flex: 0.05,
-                  display: "grid",
-                  justifyContent: "center"
+                  fontSize: "10px",
+                  color: "grey"
                 }}
               >
-                <div>
-                  <img
-                    src={image}
-                    style={{ width: "35px", height: "35px", margin: 10 }}
-                    alt="avatar"
-                  />
-                </div>
-                <div
-                  style={{
-                    fontSize: "10px",
-                    color: "grey"
-                  }}
-                >
-                  JOHN DOE
-                </div>
+                JOHN DOE
               </div>
-            </ListItem>
-            <Divider />
-            <ListItem style={{ flex: 1 }}>
-              <div style={{ display: "block", flex: 0.1 }}>
-                <div style={{ textAlign: "center" }}>0</div>
-                <div style={{ textAlign: "center" }}>votes</div>
-                <div style={{ textAlign: "center" }}>0</div>
-                <div style={{ textAlign: "center" }}>answers</div>
-              </div>
-              <div style={{ flex: 0.85 }}>
-                <ListItemText
-                  primary="Question Title"
-                  secondary="lorem ipsum dolor sit amet, consetetur 
+            </div>
+          </ListItem>
+          <Divider />
+          <ListItem style={{ flex: 1 }}>
+            <div style={{ display: "block", flex: 0.1 }}>
+              <div style={{ textAlign: "center" }}>0</div>
+              <div style={{ textAlign: "center" }}>votes</div>
+              <div style={{ textAlign: "center" }}>0</div>
+              <div style={{ textAlign: "center" }}>answers</div>
+            </div>
+            <div style={{ flex: 0.85 }}>
+              <ListItemText
+                primary="Question Title"
+                secondary="lorem ipsum dolor sit amet, consetetur 
             sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
             aliquyam erat, sed diam.  lorem ipsum dolor sit amet, consetetur 
             sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
             aliquyam erat, sed diam.  lorem ipsum"
+              />
+              <div
+                style={{
+                  backgroundColor: "grey",
+                  padding: 10,
+                  width: "fit-content",
+                  margin: 5,
+                  borderRadius: 10,
+                  color: "white",
+                  fontSize: 12
+                }}
+              >
+                Web Development
+              </div>
+            </div>
+            <div
+              style={{
+                flex: 0.05,
+                display: "grid",
+                justifyContent: "center"
+              }}
+            >
+              <div>
+                <img
+                  src={image}
+                  style={{ width: "35px", height: "35px", margin: 10 }}
+                  alt="avatar"
                 />
-                <div
-                  style={{
-                    backgroundColor: "grey",
-                    padding: 10,
-                    width: "fit-content",
-                    margin: 5,
-                    borderRadius: 10,
-                    color: "white",
-                    fontSize: 12
-                  }}
-                >
-                  Web Development
-                </div>
               </div>
               <div
                 style={{
-                  flex: 0.05,
-                  display: "grid",
-                  justifyContent: "center"
+                  fontSize: "10px",
+                  color: "grey"
                 }}
               >
-                <div>
-                  <img
-                    src={image}
-                    style={{ width: "35px", height: "35px", margin: 10 }}
-                    alt="avatar"
-                  />
-                </div>
-                <div
-                  style={{
-                    fontSize: "10px",
-                    color: "grey"
-                  }}
-                >
-                  JOHN DOE
-                </div>
+                JOHN DOE
               </div>
-            </ListItem>
-            <Divider />
-          </List>
-        </div>
+            </div>
+          </ListItem>
+          <Divider />
+        </List>
       </div>
+    </div>
   );
 }
 
